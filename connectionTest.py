02e203a7abd3450e41deef86001c9a07dcda099c
AW <roboFiddle@users.noreply.github.com>
2018-08-01 23:27:39 -0400

    Update connectionTest.py

--- connectionTest.py
+++ connectionTest.py
@@ -1,6 +1,6 @@
 from connection import *
 
-myConnection = Connection("96.225.21.203", 25565)
+myConnection = Connection("174.228.136.124", 25565)
 myConnection.connect()
 print(myConnection.send(Message(MessageType.CONNECTION, 0, b'\00\00')))
 name = myConnection.message_protocol(Message(MessageType.INPUT, 0, 12598)).messageValue[4:]
