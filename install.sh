d07c988827c4c6de8509e7e294f5e9d15a34613a
David Shustin <david.shustin@gmail.com>
2018-08-18 22:28:58 -0400

    Add logging

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 #install Adafruit
+echo Installing Adafruit Libraries...
 cd /tmp
 git clone https://github.com/adafruit/Adafruit_Python_CharLCD
 cd Adafruit_Python_CharLCD
 sudo python setup.py install
 cd /
+echo Installation Complete... Deleting temporary library sources...
 sudo rm -rf /tmp/Adafruit_Python_CharLCD
 
 #Make directories if they don't already exist
+echo Creating file structure...
 cd ~
 if [ ! -d "kiosk" ]
 then
@@ -20,9 +23,12 @@ then
 	mkdir client
 fi
 
+echo Cloning sources...
 git clone https://github.com/Kiosk-Proj/Client client
 
 cd client
 
 #Add to startup script
+echo Setting startup script...
 sudo sed -i $(wc -l /etc/network/interfaces | cut -f1 -d' ')'ipython /home/pi/kiosk/client/client.py &' /etc/rc.local
+echo Installation complete!
