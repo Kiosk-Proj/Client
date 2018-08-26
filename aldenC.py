02c60725d6d70d07122d51ad25aeced7d54ee965
Alden <aldenblamp@gmail.com>
2018-08-26 16:40:05 -0400

    configuring to Javi's server

--- aldenC.py
+++ aldenC.py
@@ -6,13 +6,26 @@ class returnObj:
 		self.names = names
 		self.leaving = leaving
 
-def makeRec(userID, kiosk):
-	furl = "http://72.79.54.70:55622/acceptedID/" + userID + "/" + str(kiosk)
-	r = requests.get(url = furl, params = {})
-	rdata = r.json()
-	print(rdata)
-	isAccepted = rdata['accepted']
-	return returnObj(isAccepted, rdata['name'], rdata['leaving'])
+def makeRec(userID):
+	iniFile = open("ini.txt", "r")
+	rURL = iniFile.readline().rstrip()
+	kNum = int(iniFile.readline().rstrip())
+
+	# furl = "http://72.79.54.70:55622/acceptedID/" + userID + "/" + str(kiosk)
+	try:
+		r = requests.get(url = rURL, params = {"id":str(userID), "kiosk":str(kNum)})
+		print("finished")
+		rdata = r.json()
+		print(rdata)
+	except:
+		print("Server Error")
+		returnObj(False, "REE", False)
+
+	return returnObj(True, rdata["name"], rdata['seniorPriv'])
+	# isAccepted = rdata['accepted']
+
+	# return returnObj(isAccepted, rdata['name'], rdata['leaving'])
 
 if __name__ == '__main__':
-	makeRec("19422", 1)
+	print(makeRec("19422"))
+
