9634c1fcdc3909e5d035f8ae6b32d105259d8f38
Josh Kirshenbaum <joshua.kirshenbaum@gmail.com>
2018-08-26 16:46:22 -0400

    idk what this is

--- aldenC.py
+++ aldenC.py
@@ -1,18 +1,21 @@
 import requests
 
 class returnObj:
-	def __init__(self, works, names, leaving):
+	def __init__(self, works, names, leaving, failed=False):
 		self.works = works
 		self.names = names
 		self.leaving = leaving
+		self.failed=failed
 
 def makeRec(userID, kiosk):
 	furl = "http://72.79.54.70:55622/acceptedID/" + userID + "/" + str(kiosk)
-	r = requests.get(url = furl, params = {})
-	rdata = r.json()
-	print(rdata)
-	isAccepted = rdata['accepted']
-	return returnObj(isAccepted, rdata['name'], rdata['leaving'])
+	try:	
+		r = requests.get(url = furl, params = {})
+		rdata = r.json()
+		isAccepted = rdata['accepted']
+		return returnObj(isAccepted, rdata['name'], rdata['leaving'])
+	except:
+		return returnObj(False, "Server", False, failed=True)
 
 if __name__ == '__main__':
 	makeRec("19422", 1)
