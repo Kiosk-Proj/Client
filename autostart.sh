be6351e71ad782423396632674c6acf106a19483
Alden Lamp <aldenblamp@gmail.com>
2018-09-05 15:53:05 -0400

    Get rid of autoupdate

--- autostart.sh
+++ autostart.sh
@@ -1,16 +1,17 @@
-internet=true
-SECONDS=$(date +"%s")
-while ! ifconfig | grep "172\.20\.94\." > /dev/null;
-do
-	echo "Wifi still not connected..."
-	if [ $(($(date +"%s")-$SECONDS)) -gt 10 ]; then
-		internet=false
-		echo "Internet connection timed out."
-		break
-	fi
-done
-if $internet ;
-then
-	git -C /home/pi/kiosk/client pull
-fi
 python /home/pi/kiosk/client/client.py &
+#internet=true
+#SECONDS=$(date +"%s")
+#while ! ifconfig | grep "172\.20\.94\." > /dev/null;
+#do
+#	echo "Wifi still not connected..."
+#	if [ $(($(date +"%s")-$SECONDS)) -gt 20 ]; then
+#		internet=false
+#		echo "Internet connection timed out."
+#		break
+#	fi
+#done
+#if $internet ;
+#then
+#	git -C /home/pi/kiosk/client pull
+#fi
+#python /home/pi/kiosk/client/client.py &
